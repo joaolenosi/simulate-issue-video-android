ed0397f3d361e234d59ba40d45fe6b7976238b70
Joao Leno <joaolenosi@gmail.com>
2024-02-26 15:52:24 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
 
 export default function App() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://noepet.com/arquivo-estatico-areas/uploads/img-publicacao/user_id.otimizado.95_65dbb4899e521.mp4',
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +33,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+ 
---
+++ README.md
@@ -0,0 +1,11 @@
+# Projeto Expo Video Issue
+
+Este repositório contém um projeto Expo para demonstrar um problema de carregamento de vídeo no Android.
+
+## Para reproduzir o erro
+
+1. Clone este repositório.
+2. Execute `npm install` para instalar as dependências.
+3. Execute o projeto no seu dispositivo Android usando `npx expo start`.
+4. Acesse a tela com o player de vídeo e tente reproduzir o vídeo problemático.
+5. Link do vídeo que não funciona: https://noepet.com/arquivo-estatico-areas/uploads/img-publicacao/user_id.otimizado.95_65dbb4899e521.mp4
